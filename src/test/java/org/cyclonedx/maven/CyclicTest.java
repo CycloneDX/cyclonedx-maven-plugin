30d6af90e49962177564fc68289d4481c9fd3d7f
Hervé Boutemy <hboutemy@sonatype.com>
2023-04-16 19:17:33 +0200

    use metadata properties instead of tool name
    Signed-off-by: Hervé Boutemy <hboutemy@sonatype.com>

--- src/test/java/org/cyclonedx/maven/CyclicTest.java
+++ src/test/java/org/cyclonedx/maven/CyclicTest.java
@@ -10,7 +10,6 @@ import static org.cyclonedx.maven.TestUtils.readXML;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 

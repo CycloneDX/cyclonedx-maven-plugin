a964c6e80164914e48207239cef29e722a6e87af
Volkan Yazıcı <volkan@yazi.ci>
2024-01-19 12:05:56 +0100

    Add tests (#459)
    Signed-off-by: Volkan Yazıcı <volkan@yazi.ci>

--- src/test/java/org/cyclonedx/maven/ExternalReferenceTest.java
+++ src/test/java/org/cyclonedx/maven/ExternalReferenceTest.java
@@ -8,11 +8,12 @@ import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Collections;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
 /**
@@ -39,22 +40,51 @@ public class ExternalReferenceTest extends BaseMavenVerifier {
                 .execute("clean", "verify")
                 .assertErrorFreeLog();
 
-        // Verify parent metadata
-        assertExternalReferences(
-                new File(projDir, "target/bom.json"),
-                "$.metadata.component.externalReferences[?(@.type=='chat')].url",
-                Collections.singleton("https://acme.com/parent"));
-
-        // Verify child metadata
-        assertExternalReferences(
-                new File(projDir, "child/target/bom.json"),
-                "$.metadata.component.externalReferences[?(@.type=='chat')].url",
-                Arrays.asList("https://acme.com/parent", "https://acme.com/child"));
+        // Verify parent & child external references
+        verifyParentExternalReferences(projDir);
+        verifyChildExternalReferences(projDir);
 
     }
 
-    private static void assertExternalReferences(File bomFile, String jsonPath, Iterable<Object> expectedValues) throws IOException {
-        byte[] bomJsonBytes = Files.readAllBytes(bomFile.toPath());
+    private static void verifyParentExternalReferences(File projDir) {
+        File bomJsonFile = new File(projDir, "target/bom.json");
+        assertExternalReferences(bomJsonFile, "chat", "url", singleton("https://acme.com/parent"));
+        assertExternalReferences(bomJsonFile, "website", "url", singleton("https://cyclonedx.org/acme"));
+        assertExternalReferences(bomJsonFile, "vcs", "url", singleton("https://github.com/CycloneDX/cyclonedx-maven-plugin.git"));
+        verifyCommonExternalReferences(bomJsonFile);
+    }
+
+    private static void verifyChildExternalReferences(File projDir) {
+        File bomJsonFile = new File(projDir, "child/target/bom.json");
+        assertExternalReferences(bomJsonFile, "chat", "url", asList("https://acme.com/parent", "https://acme.com/child"));
+        assertExternalReferences(bomJsonFile, "website", "url", singleton("https://cyclonedx.org/acme/child"));
+        assertExternalReferences(bomJsonFile, "vcs", "url", singleton("https://github.com/CycloneDX/cyclonedx-maven-plugin.git/child"));
+        verifyCommonExternalReferences(bomJsonFile);
+    }
+
+    private static void verifyCommonExternalReferences(File bomJsonFile) {
+        assertExternalReferences(bomJsonFile, "chat", "comment", singleton("optional comment"));
+        assertExternalReferences(bomJsonFile, "release-notes", "url", singleton("https://github.com/CycloneDX/cyclonedx-maven-plugin/releases"));
+        assertExternalReferences(bomJsonFile, "build-system", "url", singleton("https://github.com/CycloneDX/cyclonedx-maven-plugin/actions"));
+        assertExternalReferences(bomJsonFile, "distribution", "url", singleton("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"));
+        assertExternalReferences(bomJsonFile, "issue-tracker", "url", singleton("https://github.com/CycloneDX/cyclonedx-maven-plugin/issues"));
+        assertExternalReferences(bomJsonFile, "mailing-list", "url", singleton("https://dev.ml.cyclonedx.org/archive"));
+    }
+
+    private static void assertExternalReferences(File bomJsonFile, String type, String key, Iterable<?> expectedValues) {
+        assertExternalReferences(
+                bomJsonFile,
+                "$.metadata.component.externalReferences[?(@.type=='" + type + "')]." + key,
+                expectedValues);
+    }
+
+    private static void assertExternalReferences(File bomFile, String jsonPath, Iterable<?> expectedValues) {
+        byte[] bomJsonBytes;
+        try {
+            bomJsonBytes = Files.readAllBytes(bomFile.toPath());
+        } catch (IOException error) {
+            throw new UncheckedIOException(error);
+        }
         String bomJson = new String(bomJsonBytes, StandardCharsets.UTF_8);
         assertThatJson(bomJson)
                 .inPath(jsonPath)
